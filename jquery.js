7f698df23dfdfa794f339672dc40f24886afa7fd
Jose Sundararasu <79258242+jose42098@users.noreply.github.com>
2023-04-12 16:51:45 +0530

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,12 @@
+	function answer1(){ 
+		document.getElementById('a1').innerHTML = "முற்பத்து நான்கு - 34";
+	}
+	function answer2(){ 
+		document.getElementById('a2').innerHTML = "எட்டு - 8 ";
+	}
+	function answer3(){ 
+		document.getElementById('b1').innerHTML = "எழுத்ததிகாரம் ";
+	}
+	function answer4(){ 
+		document.getElementById('b2').innerHTML = "மாத்திரை ";
+	}

c5b8e3d197a47c52ad4cfcd8dbad444541fa2347
Jose Sundararasu <79258242+jose42098@users.noreply.github.com>
2023-04-13 14:30:13 +0530

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -10,3 +10,25 @@
 	function answer4(){ 
 		document.getElementById('b2').innerHTML = "மாத்திரை ";
 	}
+	function answerc1(){ 
+		document.getElementById('c1').innerHTML = "இயலசை";
+	}
+	function answerc2(){ 
+		document.getElementById('c2').innerHTML = "இரண்டு(௨) வகைப்படும்<br> நேரசை, நிரையசை";
+	}
+	function answerc3(){ 
+		document.getElementById('c3').innerHTML = "குற்றியலிகரம்";
+	}
+	function answerc4(){ 
+		document.getElementById('c4').innerHTML = "தனிக்குறில்";
+	}
+	function answerc5(){ 
+		document.getElementById('c5').innerHTML = "நிரையசை";
+	}
+	function answerc6(){ 
+		document.getElementById('c6').innerHTML = "நேர்பும், நிரைபும்";
+	}
+	function answerc7(){ 
+		document.getElementById('c7').innerHTML = "வேண்டு-நேர்பசை<br>வேண்டும்<br>வேண்-நேர், டும்-நேர்";
+	}
+	

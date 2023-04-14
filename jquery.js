1c76d338a75e72559cab653ca00825dcec9a8e40
Jose Sundararasu <79258242+jose42098@users.noreply.github.com>
2023-04-15 01:26:56 +0530

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -4,12 +4,14 @@
 	function answer2(){ 
 		document.getElementById('a2').innerHTML = "எட்டு - 8 ";
 	}
+
 	function answer3(){ 
 		document.getElementById('b1').innerHTML = "எழுத்ததிகாரம் ";
 	}
 	function answer4(){ 
 		document.getElementById('b2').innerHTML = "மாத்திரை ";
 	}
+
 	function answerc1(){ 
 		document.getElementById('c1').innerHTML = "இயலசை";
 	}
@@ -31,4 +33,55 @@
 	function answerc7(){ 
 		document.getElementById('c7').innerHTML = "வேண்டு-நேர்பசை<br>வேண்டும்<br>வேண்-நேர், டும்-நேர்";
 	}
+
+	function answerd1(){ 
+		document.getElementById('d1').innerHTML = "இயற்சீர்";
+	}
+	function answerd2(){ 
+		document.getElementById('d2').innerHTML = "தா-மரை<br>சே-வடி";
+	}
+	function answerd3(){ 
+		document.getElementById('d3').innerHTML = "60";
+	}
+	function answerd4(){ 
+		document.getElementById('d4').innerHTML = "ஆசிரிய உரிச்சீர்";
+	}
+	function answerd5(){ 
+		document.getElementById('d5').innerHTML = "சீர்";
+	}
+
+
+	function answere1(){ 
+		document.getElementById('e1').innerHTML = "அடி";
+	}
+	function answere2(){ 
+		document.getElementById('e2').innerHTML = "பாட்டு";
+	}
+	function answere3(){ 
+		document.getElementById('e3').innerHTML = "நேரடி";
+	}
+	function answere4(){ 
+		document.getElementById('e4').innerHTML = "குறளடி";
+	}
+	function answere5(){ 
+		document.getElementById('e5').innerHTML = "சிற்றெல்லை-10 <br> பேரெல்லை-14";
+	}
+	function answere6(){ 
+		document.getElementById('e6').innerHTML = "மூன்று (௩)";
+	}
+	function answere7(){ 
+		document.getElementById('e7').innerHTML = "சிந்தடி";
+	}
+	function answere8(){ 
+		document.getElementById('e8').innerHTML = "தவறு";
+	}
 	
+	function answerf1(){ 
+		document.getElementById('f1').innerHTML = "விடுகதை";
+	}
+	function answerf2(){ 
+		document.getElementById('f2').innerHTML = "பாட்டு, உரை, நூல், வாய்மொழி. பிசி,அங்கதம், முதுமொழி";
+	}
+	function answerf3(){ 
+		document.getElementById('f3').innerHTML = "கட்டுதல்";
+	}

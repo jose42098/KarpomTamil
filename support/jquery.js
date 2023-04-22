ecb91940dd3e8d5aa9e849dae65267239c40221a
Jose Sundararasu <79258242+jose42098@users.noreply.github.com>
2023-04-23 01:50:41 +0530

    Add files via upload

---
+++ support/jquery.js
@@ -0,0 +1,219 @@
+	function allAns(){
+		answer1(); answer2(); answer3(); answer4();
+		answerc1(); answerc2(); answerc3(); answerc4(); answerc5(); answerc6(); answerc7();
+		answerd1(); answerd2(); answerd3(); answerd4(); answerd5();
+		answere1(); answere2(); answere3(); answere4(); answere5(); answere6(); answere7(); answere8();
+		answerf1(); answerf2(); answerf3();
+		answerg1(); answerg2();
+		answerh1(); answerh2(); answerh3();
+		answeri1(); answeri2(); answeri3(); answeri4();
+		answerj1(); answerj2(); answerj3(); answerj4();
+		answerk1(); answerk2(); answerk3(); answerk4(); answerk5(); answerk6(); answerk7(); answerk8(); answerk9();
+		answerl1(); answerl2();
+		answerm1();
+		answern1(); answern2(); answern3(); answern4(); answern5();
+		answero1(); answero2(); answero3(); answero4(); answero5();
+		window.print();
+	}
+	
+	function answer1(){ 
+		document.getElementById('a1').innerHTML = "முற்பத்து நான்கு - 34";
+	}
+	function answer2(){ 
+		document.getElementById('a2').innerHTML = "எட்டு - 8 ";
+	}
+
+	function answer3(){ 
+		document.getElementById('b1').innerHTML = "எழுத்ததிகாரம் ";
+	}
+	function answer4(){ 
+		document.getElementById('b2').innerHTML = "மாத்திரை ";
+	}
+
+	function answerc1(){ 
+		document.getElementById('c1').innerHTML = "இயலசை";
+	}
+	function answerc2(){ 
+		document.getElementById('c2').innerHTML = "இரண்டு(௨) வகைப்படும்<br> நேரசை, நிரையசை";
+	}
+	function answerc3(){ 
+		document.getElementById('c3').innerHTML = "குற்றியலிகரம்";
+	}
+	function answerc4(){ 
+		document.getElementById('c4').innerHTML = "தனிக்குறில்";
+	}
+	function answerc5(){ 
+		document.getElementById('c5').innerHTML = "நிரையசை";
+	}
+	function answerc6(){ 
+		document.getElementById('c6').innerHTML = "நேர்பும், நிரைபும்";
+	}
+	function answerc7(){ 
+		document.getElementById('c7').innerHTML = "வேண்டு-நேர்பசை<br>வேண்டும்<br>வேண்-நேர், டும்-நேர்";
+	}
+
+	function answerd1(){ 
+		document.getElementById('d1').innerHTML = "இயற்சீர்";
+	}
+	function answerd2(){ 
+		document.getElementById('d2').innerHTML = "தா-மரை<br>சே-வடி";
+	}
+	function answerd3(){ 
+		document.getElementById('d3').innerHTML = "60";
+	}
+	function answerd4(){ 
+		document.getElementById('d4').innerHTML = "ஆசிரிய உரிச்சீர்";
+	}
+	function answerd5(){ 
+		document.getElementById('d5').innerHTML = "சீர்";
+	}
+
+
+	function answere1(){ 
+		document.getElementById('e1').innerHTML = "அடி";
+	}
+	function answere2(){ 
+		document.getElementById('e2').innerHTML = "பாட்டு";
+	}
+	function answere3(){ 
+		document.getElementById('e3').innerHTML = "நேரடி";
+	}
+	function answere4(){ 
+		document.getElementById('e4').innerHTML = "குறளடி";
+	}
+	function answere5(){ 
+		document.getElementById('e5').innerHTML = "சிற்றெல்லை-10 <br> பேரெல்லை-14";
+	}
+	function answere6(){ 
+		document.getElementById('e6').innerHTML = "மூன்று (௩)";
+	}
+	function answere7(){ 
+		document.getElementById('e7').innerHTML = "சிந்தடி";
+	}
+	function answere8(){ 
+		document.getElementById('e8').innerHTML = "தவறு";
+	}
+	
+	function answerf1(){ 
+		document.getElementById('f1').innerHTML = "விடுகதை";
+	}
+	function answerf2(){ 
+		document.getElementById('f2').innerHTML = "பாட்டு, உரை, நூல், வாய்மொழி, பிசி, அங்கதம், முதுமொழி";
+	}
+	function answerf3(){ 
+		document.getElementById('f3').innerHTML = "கட்டுதல்";
+	}
+
+	function answerg1(){ 
+		document.getElementById('g1').innerHTML = "நான்கு";
+	}
+	function answerg2(){ 
+		document.getElementById('g2').innerHTML = "இயற்சொல், திரிசொல், திசைச்சொல், வடசொல்";
+	}
+
+	function answerh1(){ 
+		document.getElementById('h1').innerHTML = "அகவல்";
+	}
+	function answerh2(){ 
+		document.getElementById('h2').innerHTML = "கலிப்பா";
+	}
+	function answerh3(){ 
+		document.getElementById('h3').innerHTML = "சரி";
+	}
+
+	function answeri1(){ 
+		document.getElementById('i1').innerHTML = "நான்கு (௪)";
+	}
+	function answeri2(){ 
+		document.getElementById('i2').innerHTML = "கிளை";
+	}
+	function answeri3(){ 
+		document.getElementById('i3').innerHTML = "முரண்";
+	}
+	function answeri4(){ 
+		document.getElementById('i4').innerHTML = "13699";
+	}
+
+	function answerj1(){ 
+		document.getElementById('j1').innerHTML = "மோனை";
+	}
+	function answerj2(){ 
+		document.getElementById('j2').innerHTML = "இரட்டைத்தொடை";
+	}
+	function answerj3(){ 
+		document.getElementById('j3').innerHTML = "எதுகை";
+	}
+	function answerj4(){ 
+		document.getElementById('j4').innerHTML = "இரும்பின் அன்ன.. <br> பொன்னின் அன்ன..";
+	}
+
+	function answerk1(){ 
+		document.getElementById('k1').innerHTML = "ஆயிரம்";
+	}
+	function answerk2(){ 
+		document.getElementById('k2').innerHTML = "பன்னிரண்டு";
+	}
+	function answerk3(){ 
+		document.getElementById('k3').innerHTML = "இருபத்து ஐந்து";
+	}
+	function answerk4(){ 
+		document.getElementById('k4').innerHTML = "விடுகதை";
+	}
+	function answerk5(){ 
+		document.getElementById('k5').innerHTML = "பிண்டம்";
+	}
+	function answerk6(){ 
+		document.getElementById('k6').innerHTML = "நான்கு";
+	}
+	function answerk7(){ 
+		document.getElementById('k7').innerHTML = "பண்ணத்தி";
+	}
+	function answerk8(){ 
+		document.getElementById('k8').innerHTML = "சூத்திரம்";
+	}
+	function answerk9(){ 
+		document.getElementById('k9').innerHTML = "ஓத்து";
+	}
+
+	function answerl1(){ 
+		document.getElementById('l1').innerHTML = "கைகோள்";
+	}
+	function answerl2(){ 
+		document.getElementById('l2').innerHTML = "கற்பு";
+	}
+
+	function answerm1(){ 
+		document.getElementById('m1').innerHTML = "பார்ப்பான், பாங்கன், தோழி, செவிலி, தலைவன், தலைவி";
+	}
+
+	function answern1(){ 
+		document.getElementById('n1').innerHTML = "3";
+	}
+	function answern2(){ 
+		document.getElementById('n2').innerHTML = "இறப்பு, நிகழ்வு, எதிர்";
+	}
+	function answern3(){ 
+		document.getElementById('n3').innerHTML = "பயன்";
+	}
+	function answern4(){ 
+		document.getElementById('n4').innerHTML = "8";
+	}
+	function answern5(){ 
+		document.getElementById('n5').innerHTML = "நகை, அழுகை, இளிவரல், மருட்கை, அச்சம், பெருமிதம், வெகுளி, உவகை";
+	}
+	
+	function answero1(){ 
+		document.getElementById('o1').innerHTML = "இழைபு";
+	}
+	function answero2(){ 
+		document.getElementById('o2').innerHTML = "அழகு";
+	}
+	function answero3(){ 
+		document.getElementById('o3').innerHTML = "எட்டு";
+	}
+	function answero4(){ 
+		document.getElementById('o4').innerHTML = "பழமை";
+	}
+	function answero5(){ 
+		document.getElementById('o5').innerHTML = "விருந்து";
+	}
